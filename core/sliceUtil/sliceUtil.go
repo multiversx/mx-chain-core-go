30bedcccf643595f798b6e9e15515569741da705
Marius <marius.ciucam@gmail.com>
2021-11-05 17:17:29 +0200

    FEAT: Add IsIndexSetInBitmap + tests

--- core/sliceUtil/sliceUtil.go
+++ core/sliceUtil/sliceUtil.go
@@ -9,3 +9,17 @@ func TrimSliceSliceByte(in [][]byte) [][]byte {
 	copy(ret, in)
 	return ret
 }
+
+// IsIndexSetInBitmap - checks if a bit is set(1) in the given bitmap
+func IsIndexSetInBitmap(index uint32, bitmap []byte) bool {
+	indexOutOfBounds := index >= uint32(len(bitmap))*8
+	if indexOutOfBounds {
+		return false
+	}
+
+	bytePos := index / 8
+	byteInMap := bitmap[bytePos]
+	bitPos := index % 8
+	mask := uint8(1 << bitPos)
+	return (byteInMap & mask) != 0
+}
