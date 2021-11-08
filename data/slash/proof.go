400edd5ac4ab5a686c739a8761570667052f3375
Marius <marius.ciucam@gmail.com>
2021-11-08 12:27:02 +0200

    FIX: Delete proto proof files

--- data/slash/proof.go
+++
@@ -1,2 +0,0 @@
-//go:generate protoc -I=. -I=$GOPATH/src/github.com/ElrondNetwork/elrond-go-core/data/block -I=$GOPATH/src -I=$GOPATH/src/github.com/ElrondNetwork/protobuf/protobuf --gogoslick_out=. proof.proto
-package slash
