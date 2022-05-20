c33ab1a447e5f142fdee575c7d2bc763a403ea29
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-05-20 17:01:11 +0300

    Add extra fields on apiTransactionResult.

--- data/transaction/apiTransactionResult.go
+++ data/transaction/apiTransactionResult.go
@@ -11,6 +11,8 @@ import (
 type ApiTransactionResult struct {
 	Tx                                data.TransactionHandler   `json:"-"`
 	Type                              string                    `json:"type"`
+	TypeOnSource                      string                    `json:"typeOnSource"`
+	TypeOnDestination                 string                    `json:"typeOnDestination"`
 	Hash                              string                    `json:"hash,omitempty"`
 	Nonce                             uint64                    `json:"nonce,omitempty"`
 	Round                             uint64                    `json:"round,omitempty"`
