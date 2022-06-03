ecadeed5825f50d3c4f9e7aadb4178bd4121897c
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-06-03 18:04:55 +0300

    Add "AccountQueryOptions".

--- data/api/queries.go
+++ data/api/queries.go
@@ -1,5 +1,10 @@
 package api
 
+// AccountQueryOptions holds options for account queries.
+type AccountQueryOptions struct {
+	OnFinalBlock bool
+}
+
 // BlockQueryOptions holds options for block queries.
 type BlockQueryOptions struct {
 	WithTransactions bool
