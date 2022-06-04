adc6e5eb28078bfb92c931c9c63e0cf18e14cc9c
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2022-06-04 10:31:17 +0300

    Fix field.

--- data/api/blockInfo.go
+++ data/api/blockInfo.go
@@ -2,7 +2,7 @@ package api
 
 // BlockInfo is a data transfer object used on the API
 type BlockInfo struct {
-	Nonce    string `json:"nonce,omitempty"`
+	Nonce    uint64 `json:"nonce,omitempty"`
 	Hash     string `json:"hash,omitempty"`
 	RootHash string `json:"RootHash,omitempty"`
 }
