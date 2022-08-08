256bcac1fcfe8dfc3eeb12375f7a63a40db4f608
Iuga Mihai <mihai9625@gmail.com>
2022-08-08 14:32:42 +0300

    export transaction with fee structure

--- data/outport/txWithFee.go
+++ data/outport/txWithFee.go
@@ -6,43 +6,44 @@ import (
 	"github.com/ElrondNetwork/elrond-go-core/data"
 )
 
-type transactionHandlerWithGasAndFee struct {
+// TransactionHandlerWithGasAndFee hold a data.TransactionHandler and information about fee and gas used0
+type TransactionHandlerWithGasAndFee struct {
 	data.TransactionHandler
 
-	gasUsed uint64
-	fee     *big.Int
+	GasUsed uint64
+	Fee     *big.Int
 }
 
 // NewTransactionHandlerWithGasAndFee returns a new instance of transactionHandlerWithGasAndFee which matches the interface
 func NewTransactionHandlerWithGasAndFee(txHandler data.TransactionHandler, gasUsed uint64, fee *big.Int) data.TransactionHandlerWithGasUsedAndFee {
-	return &transactionHandlerWithGasAndFee{
+	return &TransactionHandlerWithGasAndFee{
 		TransactionHandler: txHandler,
-		gasUsed:            gasUsed,
-		fee:                fee,
+		GasUsed:            gasUsed,
+		Fee:                fee,
 	}
 }
 
 // SetGasUsed sets the used gas internally
-func (t *transactionHandlerWithGasAndFee) SetGasUsed(gasUsed uint64) {
-	t.gasUsed = gasUsed
+func (t *TransactionHandlerWithGasAndFee) SetGasUsed(gasUsed uint64) {
+	t.GasUsed = gasUsed
 }
 
 // GetGasUsed returns the used gas of the transaction
-func (t *transactionHandlerWithGasAndFee) GetGasUsed() uint64 {
-	return t.gasUsed
+func (t *TransactionHandlerWithGasAndFee) GetGasUsed() uint64 {
+	return t.GasUsed
 }
 
 // SetFee sets the fee internally
-func (t *transactionHandlerWithGasAndFee) SetFee(fee *big.Int) {
-	t.fee = fee
+func (t *TransactionHandlerWithGasAndFee) SetFee(fee *big.Int) {
+	t.Fee = fee
 }
 
 // GetFee returns the fee of the transaction
-func (t *transactionHandlerWithGasAndFee) GetFee() *big.Int {
-	return t.fee
+func (t *TransactionHandlerWithGasAndFee) GetFee() *big.Int {
+	return t.Fee
 }
 
 // GetTxHandler will return the TransactionHandler
-func (t *transactionHandlerWithGasAndFee) GetTxHandler() data.TransactionHandler {
+func (t *TransactionHandlerWithGasAndFee) GetTxHandler() data.TransactionHandler {
 	return t.TransactionHandler
 }
