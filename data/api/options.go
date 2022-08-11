eedf4fa888a5b4bf8e275d97f43a2ae194f17dc9
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2022-08-11 18:34:59 +0300

    New fields on AccountQueryOptions.

--- data/api/options.go
+++ data/api/options.go
@@ -4,6 +4,8 @@ package api
 type AccountQueryOptions struct {
 	OnFinalBlock   bool
 	OnStartOfEpoch uint32
+	OnBlockNonce   uint64
+	OnBlockHash    string
 }
 
 // BlockQueryOptions holds options for block queries
