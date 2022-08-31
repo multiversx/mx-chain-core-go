7057edfa2d4e8f9846af395048478719cd42731f
AdoAdoAdo <anaridado@gmail.com>
2022-08-31 17:36:37 +0300

    data: update json tag

--- data/api/guardianData.go
+++ data/api/guardianData.go
@@ -3,7 +3,7 @@ package api
 // Guardian holds the relevant information for an account guardian
 type Guardian struct {
 	Address         string `json:"address"`
-	ActivationEpoch uint32 `json:"epoch"`
+	ActivationEpoch uint32 `json:"activationEpoch"`
 }
 
 // GuardianData holds data relating to the configured guardian(s) and frozen state of an account
