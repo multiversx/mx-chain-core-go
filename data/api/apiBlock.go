886dbc4e44a356ac973243b5f3a522068132b21e
Elrond/ <marius.ciucam@gmail.com>
2022-10-18 11:46:50 +0300

    FEAT: Add hyperblock + missing dields

--- data/api/apiBlock.go
+++ data/api/apiBlock.go
@@ -54,10 +54,12 @@ type EpochStartInfo struct {
 
 // NotarizedBlock represents a notarized block
 type NotarizedBlock struct {
-	Hash  string `json:"hash"`
-	Nonce uint64 `json:"nonce"`
-	Round uint64 `json:"round"`
-	Shard uint32 `json:"shard"`
+	Hash            string   `json:"hash"`
+	Nonce           uint64   `json:"nonce"`
+	Round           uint64   `json:"round"`
+	Shard           uint32   `json:"shard"`
+	RootHash        string   `json:"rootHash"`
+	MiniBlockHashes []string `json:"miniBlockHashes,omitempty"`
 }
 
 // EpochStartShardData is a structure that holds data about the epoch start shard data
