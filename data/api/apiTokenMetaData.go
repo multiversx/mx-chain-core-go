fa775b93c69fbc3639c6ff5a7fb46a28c8184224
Elrond/ <marius.ciucam@gmail.com>
2022-11-08 12:31:10 +0200

    FEAT: Add TokenMetaData

---
+++ data/api/apiTokenMetaData.go
@@ -0,0 +1,11 @@
+package api
+
+type TokenMetaData struct {
+	Nonce      uint64   `json:"nonce"`
+	Name       string   `json:"name"`
+	Creator    string   `json:"creator"`
+	Royalties  uint32   `json:"royalties"`
+	Hash       []byte   `json:"hash"`
+	URIs       [][]byte `json:"uris"`
+	Attributes []byte   `json:"attributes"`
+}
