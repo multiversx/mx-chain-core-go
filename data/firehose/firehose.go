2da2701ae67ce97969bc2001985129e156d653b4
Elrond/ <marius.ciucam@gmail.com>
2022-12-09 16:26:06 +0200

    FIX: Refactor + missing fields

--- data/firehose/firehose.go
+++
@@ -1,3 +0,0 @@
-//go:generate protoc -I=. -I=$GOPATH/src/github.com/ElrondNetwork/elrond-go-core/data/block -I=$GOPATH/src -I=$GOPATH/src/github.com/ElrondNetwork/protobuf/protobuf --gogoslick_out=. firehose.proto
-
-package firehose
---
+++ data/firehose/firehoseBlock.go
@@ -0,0 +1,3 @@
+//go:generate protoc -I=. -I=$GOPATH/src/github.com/ElrondNetwork/elrond-go-core/data/block -I=$GOPATH/src -I=$GOPATH/src/github.com/ElrondNetwork/protobuf/protobuf --gogoslick_out=. firehoseBlock.proto
+
+package firehose
