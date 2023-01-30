efd8d1a003e6b73a68ab129460f03694ee4d9ba1
Iuga Mihai <mihai9625@gmail.com>
2023-01-30 13:20:45 +0200

    extend transaction structure

--- data/transaction/apiTransactionResult.go
+++ data/transaction/apiTransactionResult.go
@@ -25,6 +25,7 @@ type ApiTransactionResult struct {
 	ReceiverUsername                  []byte                    `json:"receiverUsername,omitempty"`
 	GasPrice                          uint64                    `json:"gasPrice,omitempty"`
 	GasLimit                          uint64                    `json:"gasLimit,omitempty"`
+	GasUsed                           uint64                    `json:"gasUsed"`
 	Data                              []byte                    `json:"data,omitempty"`
 	CodeMetadata                      []byte                    `json:"codeMetadata,omitempty"`
 	Code                              string                    `json:"code,omitempty"`
@@ -57,6 +58,7 @@ type ApiTransactionResult struct {
 	Operation                         string                    `json:"operation,omitempty"`
 	Function                          string                    `json:"function,omitempty"`
 	InitiallyPaidFee                  string                    `json:"initiallyPaidFee,omitempty"`
+	Fee                               string                    `json:"fee"`
 	IsRelayed                         bool                      `json:"isRelayed,omitempty"`
 	IsRefund                          bool                      `json:"isRefund,omitempty"`
 	CallType                          string                    `json:"callType,omitempty"`
