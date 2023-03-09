222b64e21ca6f4f7ae167d6584986b5d6d2bb930
Iuga Mihai <mihai9625@gmail.com>
2023-03-09 16:07:31 +0200

    outport block with header

--- data/outport/outportBlock.go
+++ data/outport/outportBlock.go
@@ -1,3 +1,11 @@
 //go:generate protoc -I=. -I=$GOPATH/src/github.com/multiversx/mx-chain-core-go/data/block -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf --gogoslick_out=$GOPATH/src outportBlock.proto
 
 package outport
+
+import "github.com/multiversx/mx-chain-core-go/data"
+
+// OutportBlockWithHeader will extend the OutportBlock structure
+type OutportBlockWithHeader struct {
+	OutportBlock
+	Header data.HeaderHandler
+}
