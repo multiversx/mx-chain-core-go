bc95bbc651580e6aa3511a05c358b04374140eda
Marius C <marius.ciucam@gmail.com>
2023-03-15 15:56:28 +0200

    FEAT: Add SetExecutionOrder

--- data/outport/outportBlock.go
+++ data/outport/outportBlock.go
@@ -9,3 +9,15 @@ type OutportBlockWithHeader struct {
 	*OutportBlock
 	Header data.HeaderHandler
 }
+
+func (t *TxInfo) SetExecutionOrder(order uint32) {
+	t.ExecutionOrder = order
+}
+
+func (s *SCRInfo) SetExecutionOrder(order uint32) {
+	s.ExecutionOrder = order
+}
+
+func (r *RewardInfo) SetExecutionOrder(order uint32) {
+	r.ExecutionOrder = order
+}
