40af92e279ee0aa5880f3c5b118fea70ae2e76c2
jules01 <iulian.pascalau@gmail.com>
2023-03-27 10:24:31 +0300

    - refactored switch in GetHeaderFromBytes function

---
+++ data/block/emptyBlockCreatorsContainer.go
@@ -0,0 +1,57 @@
+package block
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/multiversx/mx-chain-core-go/core"
+	"github.com/multiversx/mx-chain-core-go/core/check"
+	"github.com/multiversx/mx-chain-core-go/data"
+)
+
+var (
+	errInvalidHeaderType = errors.New("invalid header type")
+)
+
+type emptyBlockCreatorsContainer struct {
+	mut           sync.RWMutex
+	blockCreators map[core.HeaderType]EmptyBlockCreator
+}
+
+// NewEmptyBlockCreatorsContainer creates a new block creators container
+func NewEmptyBlockCreatorsContainer(creators ...EmptyBlockCreator) *emptyBlockCreatorsContainer {
+	return &emptyBlockCreatorsContainer{
+		blockCreators: make(map[core.HeaderType]EmptyBlockCreator),
+	}
+}
+
+// Add will add a new empty block creator
+func (container *emptyBlockCreatorsContainer) Add(headerType core.HeaderType, creator EmptyBlockCreator) error {
+	if check.IfNil(creator) {
+		return data.ErrNilEmptyBlockCreator
+	}
+
+	container.mut.Lock()
+	container.blockCreators[headerType] = creator
+	container.mut.Unlock()
+
+	return nil
+}
+
+// Get will try to get a new empty block creator. Errors if the empty block creator type is not found
+func (container *emptyBlockCreatorsContainer) Get(headerType core.HeaderType) (EmptyBlockCreator, error) {
+	container.mut.RLock()
+	creator, ok := container.blockCreators[headerType]
+	container.mut.RUnlock()
+
+	if !ok {
+		return nil, errInvalidHeaderType
+	}
+
+	return creator, nil
+}
+
+// IsInterfaceNil returns true if there is no value under the interface
+func (container *emptyBlockCreatorsContainer) IsInterfaceNil() bool {
+	return container == nil
+}
