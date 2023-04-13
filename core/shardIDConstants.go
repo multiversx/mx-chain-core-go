268d25e4106e2bbe38db0c33d6462e1f4792652f
SebastianMarian <sebastian.marian@elrond.com>
2023-04-13 18:48:00 +0300

    * Fixed after review

--- core/shardIDConstants.go
+++ core/shardIDConstants.go
@@ -1,5 +1,11 @@
 package core
 
+// MetachainShardId will be used to identify a shard ID as metachain
+const MetachainShardId = uint32(0xFFFFFFFF)
+
+// AllShardId will be used to identify that a message is for all shards
+const AllShardId = uint32(0xFFFFFFF0)
+
 // SovereignChainShardId will be used to identify a shard ID as sovereign chain
 const SovereignChainShardId = uint32(0xFFFFFFFE)
 
