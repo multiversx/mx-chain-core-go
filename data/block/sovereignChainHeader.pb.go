299f3345e82287c5d000a7e14397fb738ff88406
SebastianMarian <sebastian.marian@elrond.com>
2023-04-25 12:26:51 +0300

    * Refactor and improve sovereign chain header DTO and interface

--- data/block/sovereignChainHeader.pb.go
+++ data/block/sovereignChainHeader.pb.go
@@ -28,9 +28,9 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // SovereignChainHeader extends the Header structure with extra fields needed by sovereign chain
 type SovereignChainHeader struct {
-	Header                     *Header  `protobuf:"bytes,1,opt,name=Header,proto3" json:"Header,omitempty"`
-	ValidatorStatsRootHash     []byte   `protobuf:"bytes,2,opt,name=ValidatorStatsRootHash,proto3" json:"ValidatorStatsRootHash,omitempty"`
-	MainChainShardHeaderHashes [][]byte `protobuf:"bytes,3,rep,name=MainChainShardHeaderHashes,proto3" json:"MainChainShardHeaderHashes,omitempty"`
+	Header                    *Header  `protobuf:"bytes,1,opt,name=Header,proto3" json:"Header,omitempty"`
+	ValidatorStatsRootHash    []byte   `protobuf:"bytes,2,opt,name=ValidatorStatsRootHash,proto3" json:"ValidatorStatsRootHash,omitempty"`
+	ExtendedShardHeaderHashes [][]byte `protobuf:"bytes,3,rep,name=ExtendedShardHeaderHashes,proto3" json:"ExtendedShardHeaderHashes,omitempty"`
 }
 
 func (m *SovereignChainHeader) Reset()      { *m = SovereignChainHeader{} }
@@ -75,9 +75,9 @@ func (m *SovereignChainHeader) GetValidatorStatsRootHash() []byte {
 	return nil
 }
 
-func (m *SovereignChainHeader) GetMainChainShardHeaderHashes() [][]byte {
+func (m *SovereignChainHeader) GetExtendedShardHeaderHashes() [][]byte {
 	if m != nil {
-		return m.MainChainShardHeaderHashes
+		return m.ExtendedShardHeaderHashes
 	}
 	return nil
 }
@@ -89,24 +89,24 @@ func init() {
 func init() { proto.RegisterFile("sovereignChainHeader.proto", fileDescriptor_b9b8ff297a820152) }
 
 var fileDescriptor_b9b8ff297a820152 = []byte{
-	// 260 bytes of a gzipped FileDescriptorProto
+	// 263 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2a, 0xce, 0x2f, 0x4b,
 	0x2d, 0x4a, 0xcd, 0x4c, 0xcf, 0x73, 0xce, 0x48, 0xcc, 0xcc, 0xf3, 0x48, 0x4d, 0x4c, 0x49, 0x2d,
 	0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x53, 0x52, 0xba, 0xe9, 0x99, 0x25, 0x19,
 	0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9, 0xfa, 0xe9, 0xf9, 0xe9, 0xf9, 0xfa, 0x60, 0xe1, 0xa4, 0xd2,
 	0x34, 0x30, 0x0f, 0xcc, 0x01, 0xb3, 0x20, 0xba, 0xa4, 0xb8, 0x93, 0x72, 0xf2, 0x93, 0xb3, 0x21,
-	0x1c, 0xa5, 0xad, 0x8c, 0x5c, 0x22, 0xc1, 0x58, 0x6c, 0x10, 0x52, 0xe5, 0x62, 0x83, 0xb0, 0x24,
+	0x1c, 0xa5, 0xcd, 0x8c, 0x5c, 0x22, 0xc1, 0x58, 0x6c, 0x10, 0x52, 0xe5, 0x62, 0x83, 0xb0, 0x24,
 	0x18, 0x15, 0x18, 0x35, 0xb8, 0x8d, 0x78, 0x21, 0x1a, 0xf4, 0x20, 0x82, 0x41, 0x50, 0x49, 0x21,
 	0x33, 0x2e, 0xb1, 0xb0, 0xc4, 0x9c, 0xcc, 0x94, 0xc4, 0x92, 0xfc, 0xa2, 0xe0, 0x92, 0xc4, 0x92,
 	0xe2, 0xa0, 0xfc, 0xfc, 0x12, 0x8f, 0xc4, 0xe2, 0x0c, 0x09, 0x26, 0x05, 0x46, 0x0d, 0x9e, 0x20,
-	0x1c, 0xb2, 0x42, 0x76, 0x5c, 0x52, 0xbe, 0x89, 0x99, 0x10, 0x1b, 0x83, 0x33, 0x12, 0x8b, 0x52,
-	0x20, 0xe6, 0x81, 0xe4, 0x52, 0x8b, 0x25, 0x98, 0x15, 0x98, 0x35, 0x78, 0x82, 0xf0, 0xa8, 0x70,
-	0xb2, 0xbf, 0xf0, 0x50, 0x8e, 0xe1, 0xc6, 0x43, 0x39, 0x86, 0x0f, 0x0f, 0xe5, 0x18, 0x1b, 0x1e,
-	0xc9, 0x31, 0xae, 0x78, 0x24, 0xc7, 0x78, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x37,
-	0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0xf8, 0xe2, 0x91, 0x1c, 0xc3, 0x87, 0x47, 0x72, 0x8c,
-	0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x14, 0x2b, 0xd8,
-	0xfb, 0x49, 0x6c, 0x60, 0xef, 0x18, 0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0x1b, 0x5e, 0xff, 0xd4,
-	0x60, 0x01, 0x00, 0x00,
+	0x1c, 0xb2, 0x42, 0x36, 0x5c, 0x92, 0xae, 0x15, 0x25, 0xa9, 0x79, 0x29, 0xa9, 0x29, 0xc1, 0x19,
+	0x89, 0x45, 0x29, 0x10, 0xe3, 0x40, 0x52, 0xa9, 0xc5, 0x12, 0xcc, 0x0a, 0xcc, 0x1a, 0x3c, 0x41,
+	0xb8, 0x15, 0x38, 0xd9, 0x5f, 0x78, 0x28, 0xc7, 0x70, 0xe3, 0xa1, 0x1c, 0xc3, 0x87, 0x87, 0x72,
+	0x8c, 0x0d, 0x8f, 0xe4, 0x18, 0x57, 0x3c, 0x92, 0x63, 0x3c, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23,
+	0x39, 0xc6, 0x1b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x7c, 0xf1, 0x48, 0x8e, 0xe1, 0xc3,
+	0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1, 0x58, 0x8e, 0x21,
+	0x8a, 0x15, 0xec, 0xf9, 0x24, 0x36, 0xb0, 0x67, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0x09,
+	0x27, 0x59, 0x6a, 0x5e, 0x01, 0x00, 0x00,
 }
 
 func (this *SovereignChainHeader) Equal(that interface{}) bool {
@@ -134,11 +134,11 @@ func (this *SovereignChainHeader) Equal(that interface{}) bool {
 	if !bytes.Equal(this.ValidatorStatsRootHash, that1.ValidatorStatsRootHash) {
 		return false
 	}
-	if len(this.MainChainShardHeaderHashes) != len(that1.MainChainShardHeaderHashes) {
+	if len(this.ExtendedShardHeaderHashes) != len(that1.ExtendedShardHeaderHashes) {
 		return false
 	}
-	for i := range this.MainChainShardHeaderHashes {
-		if !bytes.Equal(this.MainChainShardHeaderHashes[i], that1.MainChainShardHeaderHashes[i]) {
+	for i := range this.ExtendedShardHeaderHashes {
+		if !bytes.Equal(this.ExtendedShardHeaderHashes[i], that1.ExtendedShardHeaderHashes[i]) {
 			return false
 		}
 	}
@@ -154,7 +154,7 @@ func (this *SovereignChainHeader) GoString() string {
 		s = append(s, "Header: "+fmt.Sprintf("%#v", this.Header)+",\n")
 	}
 	s = append(s, "ValidatorStatsRootHash: "+fmt.Sprintf("%#v", this.ValidatorStatsRootHash)+",\n")
-	s = append(s, "MainChainShardHeaderHashes: "+fmt.Sprintf("%#v", this.MainChainShardHeaderHashes)+",\n")
+	s = append(s, "ExtendedShardHeaderHashes: "+fmt.Sprintf("%#v", this.ExtendedShardHeaderHashes)+",\n")
 	s = append(s, "}")
 	return strings.Join(s, "")
 }
@@ -186,11 +186,11 @@ func (m *SovereignChainHeader) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.MainChainShardHeaderHashes) > 0 {
-		for iNdEx := len(m.MainChainShardHeaderHashes) - 1; iNdEx >= 0; iNdEx-- {
-			i -= len(m.MainChainShardHeaderHashes[iNdEx])
-			copy(dAtA[i:], m.MainChainShardHeaderHashes[iNdEx])
-			i = encodeVarintSovereignChainHeader(dAtA, i, uint64(len(m.MainChainShardHeaderHashes[iNdEx])))
+	if len(m.ExtendedShardHeaderHashes) > 0 {
+		for iNdEx := len(m.ExtendedShardHeaderHashes) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.ExtendedShardHeaderHashes[iNdEx])
+			copy(dAtA[i:], m.ExtendedShardHeaderHashes[iNdEx])
+			i = encodeVarintSovereignChainHeader(dAtA, i, uint64(len(m.ExtendedShardHeaderHashes[iNdEx])))
 			i--
 			dAtA[i] = 0x1a
 		}
@@ -242,8 +242,8 @@ func (m *SovereignChainHeader) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovSovereignChainHeader(uint64(l))
 	}
-	if len(m.MainChainShardHeaderHashes) > 0 {
-		for _, b := range m.MainChainShardHeaderHashes {
+	if len(m.ExtendedShardHeaderHashes) > 0 {
+		for _, b := range m.ExtendedShardHeaderHashes {
 			l = len(b)
 			n += 1 + l + sovSovereignChainHeader(uint64(l))
 		}
@@ -264,7 +264,7 @@ func (this *SovereignChainHeader) String() string {
 	s := strings.Join([]string{`&SovereignChainHeader{`,
 		`Header:` + strings.Replace(fmt.Sprintf("%v", this.Header), "Header", "Header", 1) + `,`,
 		`ValidatorStatsRootHash:` + fmt.Sprintf("%v", this.ValidatorStatsRootHash) + `,`,
-		`MainChainShardHeaderHashes:` + fmt.Sprintf("%v", this.MainChainShardHeaderHashes) + `,`,
+		`ExtendedShardHeaderHashes:` + fmt.Sprintf("%v", this.ExtendedShardHeaderHashes) + `,`,
 		`}`,
 	}, "")
 	return s
@@ -378,7 +378,7 @@ func (m *SovereignChainHeader) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 3:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field MainChainShardHeaderHashes", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field ExtendedShardHeaderHashes", wireType)
 			}
 			var byteLen int
 			for shift := uint(0); ; shift += 7 {
@@ -405,8 +405,8 @@ func (m *SovereignChainHeader) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.MainChainShardHeaderHashes = append(m.MainChainShardHeaderHashes, make([]byte, postIndex-iNdEx))
-			copy(m.MainChainShardHeaderHashes[len(m.MainChainShardHeaderHashes)-1], dAtA[iNdEx:postIndex])
+			m.ExtendedShardHeaderHashes = append(m.ExtendedShardHeaderHashes, make([]byte, postIndex-iNdEx))
+			copy(m.ExtendedShardHeaderHashes[len(m.ExtendedShardHeaderHashes)-1], dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
