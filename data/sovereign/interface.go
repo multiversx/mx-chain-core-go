8c7d8ad4ecde1db7d74ee39f05e54ef0de50c465
Marius C <marius.ciucam@gmail.com>
2023-05-04 12:54:36 +0300

    FIX: Comments

--- data/sovereign/interface.go
+++ data/sovereign/interface.go
@@ -2,6 +2,7 @@ package sovereign
 
 import "github.com/multiversx/mx-chain-core-go/data"
 
+// IncomingHeaderHandler defines the incoming header to a sovereign chain that is sent by a notifier
 type IncomingHeaderHandler interface {
 	GetIncomingLogHandlers() []data.LogHandler
 	GetHeaderHandler() data.HeaderHandler
