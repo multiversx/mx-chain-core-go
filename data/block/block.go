78d898f25096b2b27e201ef874721cf05891c3b5
Marius C <marius.ciucam@gmail.com>
2023-05-17 16:25:53 +0300

    FIX: Proto to be integrated externally + remove ImportDB

--- data/block/block.go
+++ data/block/block.go
@@ -1,4 +1,4 @@
-//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf  --gogoslick_out=. block.proto
+//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf  --gogoslick_out=$GOPATH/src block.proto
 package block
 
 import (
