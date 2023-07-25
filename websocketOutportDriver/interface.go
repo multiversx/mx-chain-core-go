ff9bc838e0821d08ae240543b75a3d18c67de6a9
ssd04 <dariussuirab@gmail.com>
2023-07-25 12:24:55 +0300

    add ws outport driver dir in root path

---
+++ websocketOutportDriver/interface.go
@@ -0,0 +1,36 @@
+package websocketOutportDriver
+
+import (
+	"github.com/multiversx/mx-chain-core-go/data"
+	"github.com/multiversx/mx-chain-core-go/data/outport"
+	outportSenderData "github.com/multiversx/mx-chain-core-go/websocketOutportDriver/data"
+)
+
+// Driver is an interface for saving node specific data to other storage.
+// This could be an elastic search index, a MySql database or any other external services.
+type Driver interface {
+	SaveBlock(args *outport.ArgsSaveBlockData) error
+	RevertIndexedBlock(header data.HeaderHandler, body data.BodyHandler) error
+	SaveRoundsInfo(roundsInfos []*outport.RoundInfo) error
+	SaveValidatorsPubKeys(validatorsPubKeys map[uint32][][]byte, epoch uint32) error
+	SaveValidatorsRating(indexID string, infoRating []*outport.ValidatorRatingInfo) error
+	SaveAccounts(blockTimestamp uint64, acc map[string]*outport.AlteredAccount, shardID uint32) error
+	FinalizedBlock(headerHash []byte) error
+	Close() error
+	IsInterfaceNil() bool
+}
+
+// WebSocketSenderHandler defines what the actions that a web socket sender should do
+type WebSocketSenderHandler interface {
+	Send(args outportSenderData.WsSendArgs) error
+	AddClient(wss outportSenderData.WSConn, remoteAddr string)
+	Close() error
+	IsInterfaceNil() bool
+}
+
+// Uint64ByteSliceConverter converts byte slice to/from uint64
+type Uint64ByteSliceConverter interface {
+	ToByteSlice(uint64) []byte
+	ToUint64([]byte) (uint64, error)
+	IsInterfaceNil() bool
+}
