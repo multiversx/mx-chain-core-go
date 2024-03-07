9e4b4a851b2d95e0c4eedd9be4e5f97ffe1fab6b
axenteoctavian <axente.octavian@gmail.com>
2024-03-07 09:31:43 +0200

    comments for exported types

--- data/sovereign/eventData.go
+++ data/sovereign/eventData.go
@@ -1,11 +1,13 @@
 package sovereign
 
+// TransferData holds the required data for a transfer to smart contract
 type TransferData struct {
 	GasLimit uint64
 	Function []byte
 	Args     [][]byte
 }
 
+// EventData holds the full event data structure
 type EventData struct {
 	Nonce uint64
 	*TransferData
--- data/sovereign/operationData.go
+++ data/sovereign/operationData.go
@@ -2,18 +2,21 @@ package sovereign
 
 import "math/big"
 
+// Operation holds the data needed for a bridge transfer
 type Operation struct {
 	Address      []byte
 	Tokens       []EsdtToken
 	TransferData *TransferData
 }
 
+// EsdtToken holds the token data
 type EsdtToken struct {
 	Identifier []byte
 	Nonce      uint64
 	Data       EsdtTokenData
 }
 
+// EsdtTokenType holds the type of the esdt token
 type EsdtTokenType uint32
 
 const (
@@ -24,6 +27,7 @@ const (
 	Invalid
 )
 
+// EsdtTokenData holds the properties for a token
 type EsdtTokenData struct {
 	TokenType  EsdtTokenType
 	Amount     *big.Int
