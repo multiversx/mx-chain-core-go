d2eae6684f855e98cdae13bd14d8b58e7607e99f
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2025-04-09 15:35:01 +0300

    remove setters for DTO

--- data/stateChange/stateChange.go
+++ data/stateChange/stateChange.go
@@ -1,17 +1,3 @@
 //go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf  --gogoslick_out=$GOPATH/src stateChange.proto
 
 package stateChange
-
-// SetTxHash will set the tx hash with a provided value
-func (sa *StateAccess) SetTxHash(txHash []byte) {
-	sa.TxHash = txHash
-}
-
-// SetIndex will set the index with a provided value
-func (sa *StateAccess) SetIndex(index int32) {
-	sa.Index = index
-}
-
-func (sa *StateAccess) SetAccountChanges(accountChanges *AccountChanges) {
-	sa.AccountChanges = accountChanges
-}
