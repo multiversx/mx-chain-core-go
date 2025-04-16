9eed7f1962773fe2a8ea382ecea90786266f2926
BeniaminDrasovean <beniamin.drasovean@elrond.network>
2025-04-16 16:34:24 +0300

    remove Type from StateAccess

--- data/stateChange/stateChange.go
+++ data/stateChange/stateChange.go
@@ -1,3 +1,12 @@
 //go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/multiversx/protobuf/protobuf  --gogoslick_out=$GOPATH/src stateChange.proto
 
 package stateChange
+
+// GetAccessTypeBasedOnOperation returns the access type based on the operation.
+func GetAccessTypeBasedOnOperation(operation Operation) ActionType {
+	if operation == GetCode || operation == GetAccount {
+		return Read
+	}
+
+	return Write
+}
