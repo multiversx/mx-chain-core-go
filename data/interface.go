1e325c24025f4aec7d5cff3216d62c8ee505c0ce
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2025-11-05 10:48:10 +0200

    added num pending miniblocks on shard data proposal

--- data/interface.go
+++ data/interface.go
@@ -280,15 +280,17 @@ type ShardDataHandler interface {
 // ShardDataProposalHandler defines the behavior of a shard data proposal
 type ShardDataProposalHandler interface {
 	GetHeaderHash() []byte
-	SetHeaderHash(headerHash []byte)
+	SetHeaderHash(headerHash []byte) error
 	GetRound() uint64
-	SetRound(round uint64)
+	SetRound(round uint64) error
 	GetNonce() uint64
-	SetNonce(nonce uint64)
+	SetNonce(nonce uint64) error
 	GetShardID() uint32
-	SetShardID(shardID uint32)
+	SetShardID(shardID uint32) error
 	GetEpoch() uint32
-	SetEpoch(epoch uint32)
+	SetEpoch(epoch uint32) error
+	GetNumPendingMiniBlocks() uint32
+	SetNumPendingMiniBlocks(numPending uint32) error
 	IsInterfaceNil() bool
 }
 
