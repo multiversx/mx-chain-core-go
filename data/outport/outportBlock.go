45aed580137ecdef6aa23f7bb2201674c9d052ac
miiu <mihai9625@gmail.com>
2025-11-10 16:23:13 +0200

    extra fields

--- data/outport/outportBlock.go
+++ data/outport/outportBlock.go
@@ -20,6 +20,7 @@ type HeaderDataWithBody struct {
 	IntraShardMiniBlocks []*block.MiniBlock
 	HeaderHash           []byte
 	HeaderProof          data.HeaderProofHandler
+	Results              map[string]*ExecutionResultsData
 }
 
 // OutportBlockWithHeaderAndBody is a wrapper for OutportBlock used for outport handler
