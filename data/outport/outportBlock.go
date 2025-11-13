347994c38d2d9b1ccaea7d587b63474646578a8f
miiu <mihai9625@gmail.com>
2025-11-13 11:32:12 +0200

    small fix

--- data/outport/outportBlock.go
+++ data/outport/outportBlock.go
@@ -20,7 +20,7 @@ type HeaderDataWithBody struct {
 	IntraShardMiniBlocks []*block.MiniBlock
 	HeaderHash           []byte
 	HeaderProof          data.HeaderProofHandler
-	Results              map[string]*ExecutionResultsData
+	Results              map[string]*ExecutionResultData
 }
 
 // OutportBlockWithHeaderAndBody is a wrapper for OutportBlock used for outport handler
