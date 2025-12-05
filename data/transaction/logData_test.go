afb81488f4d895d06ade0430b98d1faf3c4d6f76
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2025-12-05 16:34:55 +0200

    proper logData struct

--- data/transaction/logData_test.go
+++ data/transaction/logData_test.go
@@ -3,6 +3,7 @@ package transaction
 import (
 	"testing"
 
+	"github.com/multiversx/mx-chain-core-go/marshal"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,5 +20,28 @@ func TestLogData(t *testing.T) {
 	}
 	require.Equal(t, providedHash, logData.GetTxHash())
 	require.Equal(t, providedLog, logData.GetLogHandler())
-	require.Equal(t, providedLog.GetAddress(), logData.GetAddress())
+	require.Equal(t, providedLog.GetAddress(), logData.GetLogHandler().GetAddress())
+}
+
+func TestLogDataMarshal(t *testing.T) {
+	t.Parallel()
+
+	marshaller := &marshal.GogoProtoMarshalizer{}
+	providedHash := "hash"
+	providedLog := &Log{
+		Address: []byte("address"),
+	}
+	logData := &LogData{
+		Log:    providedLog,
+		TxHash: providedHash,
+	}
+
+	marshalledLogData, err := marshaller.Marshal(logData)
+	require.NoError(t, err)
+	println(string(marshalledLogData))
+
+	var logData2 LogData
+	err = marshaller.Unmarshal(&logData2, marshalledLogData)
+	require.NoError(t, err)
+	require.Equal(t, logData.GetTxHash(), logData2.GetTxHash())
 }
