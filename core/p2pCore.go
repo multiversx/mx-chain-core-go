b862c80a725a356f2836202b29f99fd03e0b99c2
Sorin Stanculeanu <sorinstanculeanu26@gmail.com>
2026-05-04 12:39:08 +0300

    int64 timestamp

--- core/p2pCore.go
+++ core/p2pCore.go
@@ -52,7 +52,7 @@ type P2PPeerInfo struct {
 	PeerSubType             P2PPeerSubType
 	ShardID                 uint32
 	PkBytes                 []byte
-	AuthenticationTimestamp uint64
+	AuthenticationTimestamp int64
 }
 
 // QueryP2PPeerInfo represents a DTO used in exporting p2p peer info after a query
